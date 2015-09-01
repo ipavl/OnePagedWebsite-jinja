3db11b0ca5084fffb3db71a25295987277d2c316
ipavl <ipavl@users.noreply.github.com>
2015-08-31 22:52:56 -0400

    Set up basic database schema for projects
    Does not support tagging of any sort as of yet.

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, Text, Date
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+db = SQLAlchemy(app)
+
+
+class Project(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, unique=False)
+    date = Column(Date, unique=False)
+    description = Column(Text, unique=False)
+
+db.create_all()
 
 
 @app.route('/')
